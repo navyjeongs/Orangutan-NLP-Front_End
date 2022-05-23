3c9b033ce5d767c5d5790914d659edf490c5b6f0
navyjeongs <wjdtjr8649@naver.com>
2022-05-23 23:55:40 +0900

    05.23 chatting css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  // 내 채팅
+  const [writeChat, setWriteChat] = useState({
+    name: "애저톤",
+    text: "",
+    id: uuidv4(),
+  });
+
+  const [receiveChats, setOnReceiveChats] = useState([]);
+
+  const onChangeText = (e) => {
+    const { value } = e.target;
+    setWriteChat({ ...writeChat, text: value });
+  };
+
+  const sendChat = (e) => {
+    e.preventDefault();
+    setOnReceiveChats([...receiveChats, writeChat]);
+    setWriteChat({ ...writeChat, id: uuidv4(), text: "" });
+  };
+
+  console.log(receiveChats);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ position: "absolute", top: "60%" }}>
+      <div
+        style={{
+          width: "600px",
+          height: "400px",
+          border: "1px solid #ddd",
+          overflow: "auto",
+        }}
+      >
+        {receiveChats.map((info) => {
+          return (
+            <div key={info.id}>
+              [{info.name}] : {info.text}, {info.id}
+            </div>
+          );
+        })}
+      </div>
+      <form onSubmit={sendChat}>
+        <input
+          style={{
+            width: "500px",
+          }}
+          type="text"
+          onChange={onChangeText}
+          value={writeChat.text}
+        ></input>
+      </form>
     </div>
   );
 }
