2477831090e1c7dbbe97248cd5176e86b7877646
navyjeongs <wjdtjr8649@naver.com>
2022-05-29 13:08:18 +0900

    [Update] 05-29 Add Orang.png, Change Login css

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,7 @@ import { Button, TextField } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { UserDispatch } from "./App";
-
+import image from "./playVideo/orang.jpg";
 const Login = () => {
   const dispatch = useContext(UserDispatch); // 전역변수
 
@@ -23,16 +23,37 @@ const Login = () => {
 
   return (
     <div>
-      <form onSubmit={submitName} style={{ textAlign: "center" }}>
+      <h2 style={{ textAlign: "center" }}>Orangutan-NLP</h2>
+      <div
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+          height: "50vh",
+          width: "50%",
+          position: "absolute",
+          left: "25%",
+          top: "20%",
+        }}
+      ></div>
+      <form
+        onSubmit={submitName}
+        style={{
+          textAlign: "center",
+          position: "absolute",
+          top: "10%",
+          left: "40%",
+          right: "40%",
+        }}
+      >
         <TextField
-          id="닉네임 설정"
-          label="Outlined"
+          id="닉네임"
+          label="닉네임"
           variant="outlined"
           value={name}
           onChange={settingName}
         />
         <div>
-          <Button type="submit">등록하기</Button>
+          <Button type="submit">설정하기</Button>
         </div>
       </form>
     </div>
